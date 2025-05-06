a1e50ac46bb3e9dd87b2fea16c2c286aab22218c
Edwin Kwok <113453281+EdwinKwok-RHC@users.noreply.github.com>
2025-05-06 12:22:44 -0400

    Fix main logic

--- ScopeMinimizedTokenizationDemo/Program.cs
+++ ScopeMinimizedTokenizationDemo/Program.cs
@@ -1,6 +1,27 @@
+using ScopeMinimizedTokenizationDemo.Tokenization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Register the tokenization service
+builder.Services.AddSingleton<ITokenizationService, TokenizationService>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+
+app.MapPost("/tokenize", (string contractAccount, string contractNumber, ITokenizationService tokenizationService) =>
+{
+    var token = tokenizationService.Tokenize("ContractAccount", contractAccount);
+    var token2 = tokenizationService.Tokenize("ContractNumber", contractNumber);
+    return Results.Ok(new { ContractAccountToken = token, ContractNumberToken = token2 });
+});
+
+app.MapPost("/detokenize", (string token, ITokenizationService tokenizationService) =>
+{
+    var value = tokenizationService.Detokenize(token);
+    return value != null ? Results.Ok(new { Value = value }) : Results.NotFound();
+});
+
 
 app.Run();
---
+++ ScopeMinimizedTokenizationDemo/Tokenization/TokenizationService.cs
@@ -0,0 +1,26 @@
+﻿namespace ScopeMinimizedTokenizationDemo.Tokenization;
+
+public interface ITokenizationService
+{
+    string Tokenize(string key, string value);
+    string Detokenize(string token);
+}
+
+public class TokenizationService : ITokenizationService
+{
+    private readonly Dictionary<string, string> _tokenStore = new();
+
+    public string Tokenize(string key, string value)
+    {
+        var token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+        token = token.Replace('+', '-').Replace('/', '_').Replace("=", "");
+        _tokenStore[token] = $"{key}:{value}";
+        return token;
+    }
+
+    public string Detokenize(string token)
+    {
+        //var val = _tokenStore[token];
+        return _tokenStore.TryGetValue(token, out var value) ? value : null;
+    }
+}
